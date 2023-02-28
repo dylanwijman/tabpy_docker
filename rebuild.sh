28feab4b1f892c6397c82c149d55d061f9536e57
dylanwijman <dylanwijman@hotmail.com>
2023-02-28 12:55:11 +0100

    Set current working dir in .sh scripts

--- rebuild.sh
+++ rebuild.sh
@@ -1,2 +1,5 @@
+#!/bin/sh
+MY_PATH=$(dirname "$0")
+cd "$MY_PATH"
 docker-compose build
 docker-compose up -d
--- start.sh
+++ start.sh
@@ -1,1 +1,4 @@
+#!/bin/sh
+MY_PATH=$(dirname "$0")
+cd "$MY_PATH"
 docker-compose up -d
