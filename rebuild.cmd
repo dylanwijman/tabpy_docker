7979e52bd4a98ac2801da1ec535fb2173a869768
dylanwijman <dylanwijman@hotmail.com>
2023-03-03 11:24:38 +0100

    Remove old container in rebuild scripts

--- rebuild.cmd
+++ rebuild.cmd
@@ -1,2 +1,4 @@
+docker container stop tabpy_docker-tabpy-1
+docker container rm tabpy_docker-tabpy-1
 docker-compose build
 docker-compose up -d
--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 MY_PATH=$(dirname "$0")
 cd "$MY_PATH"
+docker container stop tabpy_docker-tabpy-1
+docker container rm tabpy_docker-tabpy-1
 docker-compose build
 docker-compose up -d
